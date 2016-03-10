48588b6b4cba220e0dfb9a64b3af7dfb0803d3cd
Phantomical <phantom@lynches.ca>
2016-03-09 22:03:28 -0500

    loader.cpp is now unit tested

---
+++ re-unit-tests/load_func_test.cpp
@@ -0,0 +1,31 @@
+#include "catch.hpp"
+
+//Imported functions from loader.cpp in render-engine
+void* load_init(const char*);
+void* load_func(void*, const char*);
+void load_terminate(void*);
+
+TEST_CASE("functions can be loaded through the loader.cpp functions")
+{
+	void* load_state = nullptr;
+#ifdef _WIN32
+	//opengl32.dll should be present on every windows machine
+	load_state = load_init("opengl32.dll");
+#else
+	SUCCEED("This test not supported on other platforms than windows");
+	return;
+#endif
+
+	//The function must have state, it should not use global state
+	REQUIRE((load_state != nullptr));
+
+	SECTION("able to load a known function from a library")
+	{
+		//Load an OpenGL 1.1 function that should be present on every windows distribution (XP and up)
+		void* func = load_func(load_state, "glClear");
+
+		REQUIRE((func != nullptr));
+	}
+
+	load_terminate(load_state);
+}
---
+++ rendering-engine/loader.cpp
@@ -0,0 +1,45 @@
+
+#ifdef _WIN32
+#include <Windows.h>
+#include <stdlib.h>
+
+namespace
+{
+	struct win32_state
+	{
+		HMODULE module;
+	};
+
+	void* load_internal(const char* libname)
+	{
+		win32_state* st = (win32_state*)malloc(sizeof(win32_state));
+		st->module = LoadLibraryA(libname);
+		return st;
+	}
+	void terminate_internal(void* st)
+	{
+		FreeLibrary(((win32_state*)st)->module);
+		free(st);
+	}
+
+	void* load_internal(void* st, const char* funcname)
+	{
+		return GetProcAddress(((win32_state*)st)->module, funcname);
+	}
+}
+#else
+#error "this platform currently not supported"
+#endif
+
+void* load_init(const char* libname)
+{
+	return load_internal(libname);
+}
+void* load_func(void* state, const char* funcname)
+{
+	return load_internal(state, funcname);
+}
+void load_terminate(void* state)
+{
+	terminate_internal(state);
+}
