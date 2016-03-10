048193d6913f2966a49146edf2424d16ce8de46c
Phantomical <phantom@lynches.ca>
2016-03-10 17:45:07 -0500

    Non-generated backend interface methods are now tested

--- re-unit-tests/backend_called_test.cpp
+++ re-unit-tests/backend_called_test.cpp
@@ -19,16 +19,22 @@ TEST_CASE("backend methods are called correctly when the backend interface metho
 
 	REQUIRE(bck.is_valid());
 
+	auto array = std::array<const void*, 6>{};
+	for (auto& v : array)
+		v = nullptr;
+
 	TEST_BACKEND_METHOD(create_buffer, 0, nullptr, buffer_usage::STATIC_DRAW);
 	TEST_BACKEND_METHOD(create_shader, 0, nullptr);
+	TEST_BACKEND_METHOD(create_shader, 0, {});
 	TEST_BACKEND_METHOD(create_texture_2d, 0, 0, internal_format::R8, image_format::RED, data_type::UNSIGNED_BYTE, nullptr);
 	TEST_BACKEND_METHOD(create_texture_3d, 0, 0, 0, internal_format::R8, image_format::RED, data_type::UNSIGNED_BYTE, nullptr);
 	TEST_BACKEND_METHOD(create_texture_cubemap, 0, 0, internal_format::R8, image_format::RED, data_type::UNSIGNED_BYTE, nullptr);
+	TEST_BACKEND_METHOD(create_texture_cubemap, 0, 0, internal_format::R8, image_format::RED, data_type::UNSIGNED_BYTE, array)
 	TEST_BACKEND_METHOD(delete_buffer, buffer_handle());
 	TEST_BACKEND_METHOD(delete_shader, shader_handle());
 	TEST_BACKEND_METHOD(delete_texture, texture_handle());
 	TEST_BACKEND_METHOD(delete_render_target, render_target_handle());
-	TEST_BACKEND_METHOD(set_buffer_data, buffer_handle(), 0, nullptr);
+	TEST_BACKEND_METHOD(set_buffer_data, buffer_handle(), 0, nullptr)
+	TEST_BACKEND_METHOD(sync);;
 	TEST_BACKEND_METHOD(swap_buffers);
-	TEST_BACKEND_METHOD(sync);
 }
