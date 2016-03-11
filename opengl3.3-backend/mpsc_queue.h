353ad08f40d6d686d4348789ec372673d73139ab
Phantomical <phantom@lynches.ca>
2016-03-11 17:45:38 -0500

    Licensed the project under the GNU LGPL

--- opengl3.3-backend/mpsc_queue.h
+++ opengl3.3-backend/mpsc_queue.h
@@ -1,4 +1,23 @@
-#pragma once
+//Copyright (C) 2016 Sean Lynch
+/*
+	This file is part of rendering-engine.
+	
+	rendering-engine is free software : you can redistribute it and / or modify
+	it under the terms of the GNU Lesser General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+	
+	rendering-engine is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+	GNU Lesser General Public License for more details.
+	
+	You should have received a copy of the GNU Lesser General Public License
+	along with rendering-engine. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef RE_MPSC_QUEUE_H
+#define RE_MPSC_QUEUE_H
 
 #include "allocators.h"
 
@@ -169,3 +188,5 @@ namespace gldr
 #undef EXCHANGE
 	}
 }
+
+#endif
--- opengl3.3-backend/packed_array.h
+++ opengl3.3-backend/packed_array.h
@@ -1,3 +1,21 @@
+//Copyright (c) 2016 Sean Lynch
+/*
+	This file is part of rendering-engine.
+	
+	rendering-engine is free software : you can redistribute it and / or modify
+	it under the terms of the GNU Lesser General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+	
+	rendering-engine is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+	GNU Lesser General Public License for more details.
+	
+	You should have received a copy of the GNU Lesser General Public License
+	along with rendering-engine. If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #ifndef GLDR_PACKED_ARRAY_H
 #define GLDR_PACKED_ARRAY_H
 
